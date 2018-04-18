237b706e36c30d2cbcceb663acbf74d5ff2df2d4
Ivan Kavaliou <jownkiv@gmail.com>
2018-04-18 22:06:05 +0300

    collect was implemented

--- toa_collector/toa_collector/toa_facade.py
+++ toa_collector/toa_collector/toa_facade.py
@@ -1,7 +1,23 @@
+import time
+
 import requests
 from config import Config
-from dal import DAL
+from endpoints import toa
+from model import GetEventsResult
+from utils import ts_to_iso_8601
 
 
-def collect():
-    pass
+def collect(ts_start, ts_stop=time.time()):
+    get_events_url = toa['getEvents'].format(
+        root=Config.get_toa_url(),
+        start=ts_to_iso_8601(ts_start),
+        stop=ts_to_iso_8601(ts_stop)
+    )
+
+    response = requests.get(get_events_url)
+
+    if response.status_code == 200:
+        events = response.json()
+        return GetEventsResult(ts_start, ts_stop, events)
+    else:
+        return None
