71dc2a6d93e3509267b351f34bc02804a1237ead
Ivan Kavaliou <jownkiv@gmail.com>
2018-05-02 00:13:11 +0300

    template for frontend was added

--- nextgisweb_groza/nextgisweb_groza/view/__init__.py
+++ nextgisweb_groza/nextgisweb_groza/view/__init__.py
@@ -4,6 +4,14 @@ from last_update import *
 from settings import *
 
 
+def frontend(request):
+    return {
+        'grozaConfig': {
+            'ngwUrl': request.application_url
+        }
+    }
+
+
 def setup_pyramid(comp, config):
     config.add_route(
         'pyramid.control_panel.groza',
@@ -14,3 +22,8 @@ def setup_pyramid(comp, config):
         'nextgisweb_groza.last_update',
         '/groza/last-update'
     ).add_view(get_last_update, renderer='json')
+
+    config.add_route(
+        'nextgisweb_groza.frontend',
+        '/groza'
+    ).add_view(frontend, renderer='nextgisweb_groza:template/frontend.mako')
