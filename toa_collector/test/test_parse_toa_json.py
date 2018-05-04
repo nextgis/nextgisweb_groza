641ef5d35c96a1fbd768244c53053d73d5b5bdd8
Ivan Kavaliou <jownkiv@gmail.com>
2018-05-04 22:29:21 +0300

    modified test for using zipped example json file

--- toa_collector/test/test_parse_toa_json.py
+++ toa_collector/test/test_parse_toa_json.py
@@ -1,11 +1,16 @@
 import unittest
 from toa_collector.schemas import ToaEventsSetSchema
+from zipfile import ZipFile
+import fnmatch
 
 
 class TestParseToaJson(unittest.TestCase):
     def setUp(self):
-        with open('toa_example.json', 'r') as toa_example_file:
-            self.toa_json_string=toa_example_file.read().replace('\n', '')
+        toa_example_file_zip = ZipFile('toa_example.json.zip', 'r')
+        for name in toa_example_file_zip.namelist():
+            if fnmatch.fnmatch(name, 'toa_example.json'):
+                ex_file = toa_example_file_zip.open(name)
+                self.toa_json_string = ex_file.read()
 
     def test_should_parse_toa_set_events_json(self):
         result = ToaEventsSetSchema().loads(self.toa_json_string)
