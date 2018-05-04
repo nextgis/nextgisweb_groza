58571cab84f813f6a183f45a39c2e940e6a05978
Ivan Kavaliou <jownkiv@gmail.com>
2018-05-04 23:35:22 +0300

    post_load methods for converting to EventSchema json were added

--- toa_collector/toa_collector/schemas/toa.py
+++ toa_collector/toa_collector/schemas/toa.py
@@ -1,11 +1,19 @@
-from datetime import date
-from marshmallow import Schema, fields, pprint
+import time
+from marshmallow import Schema, fields, post_load
 
 
 class ToaErrorEllipse(Schema):
     semiMajorAxis = fields.Float()
     semiMinorAxis = fields.Float()
     azimuth = fields.Float()
+
+    @post_load
+    def post_load(self, item):
+        return dict(
+            ell_maj=item['semiMajorAxis'],
+            ell_min=item['semiMinorAxis'],
+            ell_az=item['azimuth']
+        )
 
 
 class ToaLocationSchema(Schema):
@@ -23,6 +31,34 @@ class ToaEventSchema(Schema):
     algorithmId = fields.String()
     amplitude = fields.Number()
     location = fields.Nested(ToaLocationSchema)
+    locationErrorEllipse = fields.Nested(ToaErrorEllipse)
+
+    @post_load
+    def post_load(self, item):
+        amplitude = item['amplitude']
+
+        if amplitude == 0:
+            ligh_t = 0
+        elif amplitude > 0:
+            ligh_t = 1
+        elif amplitude < 0:
+            ligh_t = 2
+        else:
+            ligh_t = None
+
+        event = dict(
+            id=item['eventId'],
+            ligh_t=ligh_t,
+            lm_ts=int(time.mktime(item['lastModifiedDateTime'].timetuple())),
+            ev_ts=int(time.mktime(item['eventDateTime'].timetuple())),
+            ampl=item['amplitude']
+        )
+
+        result = event.copy()
+        result.update(item['location'])
+        result.update(item['locationErrorEllipse'])
+
+        return result
 
 
 class ToaEventsSetSchema(Schema):
