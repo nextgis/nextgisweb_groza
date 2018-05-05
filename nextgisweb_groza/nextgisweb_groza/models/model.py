33d3e22b2ed9db8291a13765a42d00aefa0e021e
Ivan Kavaliou <jownkiv@gmail.com>
2018-05-05 22:30:58 +0300

    get_last_update was implemented

--- nextgisweb_groza/nextgisweb_groza/models/model.py
+++ nextgisweb_groza/nextgisweb_groza/models/model.py
@@ -1,11 +1,7 @@
-import json
-
 import geoalchemy2 as ga
-from sqlalchemy.ext.declarative import DeclarativeMeta
-from sqlalchemy.dialects.postgresql import ENUM
-
 from nextgisweb import db
 from nextgisweb.models import declarative_base
+from sqlalchemy.dialects.postgresql import ENUM
 
 Base = declarative_base()
 
@@ -13,7 +9,7 @@ Base = declarative_base()
 class Events(Base):
     __tablename__ = 'groza_events'
 
-    event_id = db.Column(db.Unicode, unique=True, primary_key=True)
+    event_id = db.Column(db.Unicode, primary_key=True)
     event_type = db.Column(ENUM('Lighting', 'Other', name='groza_event_types'), nullable=False)
     lighting_type = db.Column(db.Integer)  # 0 - cloud, 1 - positive ground, 2 - negative ground
     amplitude = db.Column(db.Integer)
@@ -23,3 +19,10 @@ class Events(Base):
     ellipse_major_ax = db.Column(db.DECIMAL)
     ellipse_minor_ax = db.Column(db.DECIMAL)
     ellipse_azimuth = db.Column(db.DECIMAL)
+
+
+class Meta(Base):
+    __tablename__ = 'groza_meta'
+
+    key = db.Column(db.Unicode, primary_key=True)
+    value = db.Column(db.Unicode, primary_key=True)
--- nextgisweb_groza/nextgisweb_groza/view/last_update.py
+++ nextgisweb_groza/nextgisweb_groza/view/last_update.py
@@ -1,8 +1,25 @@
 # -*- coding: utf-8 -*-
-import json
-from pyramid.response import Response
-from pyramid.view import view_config
+import transaction
+from nextgisweb.models import DBSession
+from nextgisweb_groza.models import Meta
+from sqlalchemy.orm.exc import NoResultFound
+
+LAST_UPDATE_KEY = 'LAST_UPDATE'
 
 
 def get_last_update(request):
-    return 1522611593
+    try:
+        last_update = Meta.filter_by(key=LAST_UPDATE_KEY).one()
+    except NoResultFound:
+        from datetime import datetime, timedelta
+        import calendar
+        prevent_date = datetime.utcnow() - timedelta(days=1)
+        prevent_date_utc_ts = calendar.timegm(prevent_date.utctimetuple())
+        with transaction.manager:
+            last_update = Meta(
+                key=LAST_UPDATE_KEY,
+                value=prevent_date_utc_ts
+            )
+            DBSession.add(last_update)
+        last_update = Meta.filter_by(key=LAST_UPDATE_KEY).one()
+    return int(last_update.value)
