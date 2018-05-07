da70df0b7b129b83fba7bcc950768f99a1f632d1
Ivan Kavaliou <jownkiv@gmail.com>
2018-05-07 14:24:07 +0300

    conditional statement was optimized

--- toa_collector/toa_collector/ngw_facade.py
+++ toa_collector/toa_collector/ngw_facade.py
@@ -19,10 +19,5 @@ class NgwFacade():
     @staticmethod
     def send_events_to_ngw(get_events_result):
         url = ngw['receiveEvents'].format(root=NgwFacade.config.get_ngw_url())
-
         response = requests.post(url, None, get_events_result.to_json())
-
-        if response.status_code == 200:
-            return True
-        else:
-            return False
+        return response.status_code == 200
