871f39a966cc726ce12f6501b72278c5867b8422
Ivan Kavaliou <jownkiv@gmail.com>
2018-05-11 00:01:35 +0300

    new parameters were added to config

--- toa_collector/toa_collector/config.py
+++ toa_collector/toa_collector/config.py
@@ -9,6 +9,9 @@ class Config():
     toa_url = None
     ngw_url = None
     rg_url = None
+    repeat_delay = None
+    chunk_pulling_period = None
+    active_monitoring_period = None
 
     @staticmethod
     def init():
@@ -29,7 +32,7 @@ class Config():
             url = url[:-1]
         Config.toa_url = url
         return Config.toa_url
-    
+
     @staticmethod
     def get_ngw_url():
         if Config.ngw_url:
@@ -49,3 +52,27 @@ class Config():
             url = url[:-1]
         Config.rg_url = url
         return Config.rg_url
+
+    @staticmethod
+    def get_repeat_delay():
+        if Config.repeat_delay:
+            return Config.repeat_delay
+        repeat_delay = Config.config.get(section_name, 'repeat_delay_sec')
+        Config.repeat_delay = int(repeat_delay)
+        return Config.repeat_delay
+
+    @staticmethod
+    def get_chunk_pulling_period():
+        if Config.chunk_pulling_period:
+            return Config.chunk_pulling_period
+        chunk_pulling_period = Config.config.get(section_name, 'chunk_pulling_period_sec')
+        Config.chunk_pulling_period = int(chunk_pulling_period)
+        return Config.chunk_pulling_period
+
+    @staticmethod
+    def get_active_monitoring_period():
+        if Config.active_monitoring_period:
+            return Config.active_monitoring_period
+        active_monitoring_period = Config.config.get(section_name, 'active_monitoring_period_sec')
+        Config.active_monitoring_period = int(active_monitoring_period)
+        return Config.active_monitoring_period
