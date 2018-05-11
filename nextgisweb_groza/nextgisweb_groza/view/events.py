ab7cff908b17246a0dc787569c1fbf31c7128de7
Ivan Kavaliou <jownkiv@gmail.com>
2018-05-11 11:08:30 +0300

    code was clarified

--- nextgisweb_groza/nextgisweb_groza/view/events.py
+++ nextgisweb_groza/nextgisweb_groza/view/events.py
@@ -9,11 +9,15 @@ from pyramid.exceptions import HTTPBadRequest
 
 def receive_events(request):
     json_body = request.json_body
-    parsed_events_set = EventsSetSchema().load(json_body)
-    if parsed_events_set.errors.keys() > 0:
+    parsed_events_set = EventsSetSchema().loads(json_body)
+    if len(parsed_events_set.errors.keys()) > 0:
         raise HTTPBadRequest()
-    last_update_ts = parsed_events_set.data['stop']
-    events = parsed_events_set.data['events']
+    last_update_ts = int(parsed_events_set.data['stop'])
+
+    events = []
+    if 'data' in parsed_events_set.data:
+        events = parsed_events_set.data['data']
+
     with transaction.manager:
         Meta.filter_by(key=LAST_UPDATE_KEY).update({
             'value': last_update_ts
@@ -22,5 +26,7 @@ def receive_events(request):
             DBSession.add(event)
     return response_ok(dict(
         count=len(events),
+        start=parsed_events_set.data['start'],
+        stop=parsed_events_set.data['stop'],
         lastUpdateTs=last_update_ts
     ))
