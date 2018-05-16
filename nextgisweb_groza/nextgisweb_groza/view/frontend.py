424ee11628f8e9a3f62ed21967f6f0337fe74866
Ivan Kavaliou <jownkiv@gmail.com>
2018-05-16 14:55:37 +0300

    'rgPath' setting was added

--- nextgisweb_groza/nextgisweb_groza/view/frontend.py
+++ nextgisweb_groza/nextgisweb_groza/view/frontend.py
@@ -3,11 +3,13 @@ from settings import get_settings
 
 def frontend(request):
     settings = get_settings()
+    config_settings = request.env.groza.settings
 
     return {
         'grozaConfig': {
             'ngwUrl': request.application_url,
-            'rgUrl': request.env.groza.settings['rg_url'],
+            'rgUrl': config_settings['rg_url'],
+            'rgPath': config_settings['rg_path'],
             'settings': settings
         }
     }
