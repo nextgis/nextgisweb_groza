b412692d32a6a2aaa89aa708ef1ff9419520cd13
Ivan Kavaliou <jownkiv@gmail.com>
2018-06-02 23:28:29 +0300

    zones selectors was added to Groza settings

--- nextgisweb_groza/nextgisweb_groza/amd/ngw-groza/settings/GrozaSettings.js
+++ nextgisweb_groza/nextgisweb_groza/amd/ngw-groza/settings/GrozaSettings.js
@@ -51,19 +51,27 @@ define([
                 handleAs: 'json'
             }).then(function (result) {
                 data = result.data;
-                if (data.web_map) {
-                    self.webMap.attr('value', {'id': data.web_map});
-                }
+                if (data.web_map) self.webMap.attr('value', {'id': data.web_map});
+                if (data.zone_1_class) self.zoneFirstClass.attr('value', {'id': data.zone_1_class});
+                if (data.zone_2_class) self.zoneSecondClass.attr('value', {'id': data.zone_2_class});
+                if (data.zone_3_class) self.zoneFirstClass.attr('value', {'id': data.zone_3_class});
+
                 self.ellipseZoomVisible.attr('value', data.ellipse_z_visible);
             });
         },
 
         save: function () {
+            if (!this.validate()) {
+                return false;
+            }
             xhr.put(API_URL, {
                 handleAs: 'json',
                 headers: {'Content-Type': 'application/json'},
                 data: json.stringify({
                     web_map: this.webMap.attr('value').id,
+                    zone_1_class: this.zoneFirstClass.attr('value').id,
+                    zone_2_class: this.zoneSecondClass.attr('value').id,
+                    zone_3_class: this.zoneThirdClass.attr('value').id,
                     ellipse_z_visible: this.ellipseZoomVisible.attr('value')
                 })
             }).then(function () {
@@ -71,6 +79,26 @@ define([
             }, function () {
                 alert(i18n.gettext('Error, Groza settings not has been saved!'));
             });
+        },
+
+        validate: function () {
+            if (!this.webMap.attr('value')) {
+                alert(i18n.gettext('"Web map" is required field! Please, check it.'));
+                return false;
+            }
+            if (!this.zoneFirstClass.attr('value')) {
+                alert(i18n.gettext('"Zone 1 class" is required field! Please, check it.'));
+                return false;
+            }
+            if (!this.zoneSecondClass.attr('value')) {
+                alert(i18n.gettext('"Zone 2 class" is required field! Please, check it.'));
+                return false;
+            }
+            if (!this.zoneThirdClass.attr('value')) {
+                alert(i18n.gettext('"Zone 3 class" is required field! Please, check it.'));
+                return false;
+            }
+            return true;
         }
     });
 });
