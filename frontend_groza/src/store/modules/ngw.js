399d509d6e880b9786cfdb0c9464386c4df39284
Ivan Kavaliou <jownkiv@gmail.com>
2018-06-04 13:12:52 +0300

    GET_NGW_TILE_ADAPTER_URL action was added

--- frontend_groza/src/store/modules/ngw.js
+++ frontend_groza/src/store/modules/ngw.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import {IMAGE_ADAPTER_URL, NGW_GET_EVENTS, NGW_LOGIN, NGW_WEB_MAP} from '../actions/ngw'
+import {IMAGE_ADAPTER_URL, NGW_GET_EVENTS, NGW_LOGIN, NGW_WEB_MAP, GET_NGW_TILE_ADAPTER_URL} from '../actions/ngw'
 import qs from 'qs'
 
 const state = {}
@@ -23,6 +23,10 @@ const actions = {
   [IMAGE_ADAPTER_URL]: ({commit, dispatch}) => {
     const baseUrl = http.defaults.baseURL
     return `${baseUrl}/api/component/render/image`
+  },
+  [GET_NGW_TILE_ADAPTER_URL]: ({commit, dispatch}) => {
+    const baseUrl = http.defaults.baseURL
+    return `${baseUrl}/api/component/render/tile?z={z}&x={x}&y={y}&resource={resourceId}`
   },
   [NGW_GET_EVENTS]: ({commit, dispatch}, params) => {
     return http.get(`/api/groza/events/`, {
